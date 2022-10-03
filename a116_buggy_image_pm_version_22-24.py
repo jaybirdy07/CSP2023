60a88ad241566f75a21ded4b0f27495f21aa286d
jaybirdy07 <77217029+jaybirdy07@users.noreply.github.com>
2022-10-03 09:46:37 -0700

    Update a116_buggy_image_pm_version_22-24.py

--- a116_buggy_image_pm_version_22-24.py
+++ a116_buggy_image_pm_version_22-24.py
@@ -1,1 +1,25 @@
-f
+#   a116_buggy_image.py
+import turtle as trtl
+spider = trtl.Turtle()
+
+# Create a spider body
+spider.pensize(40)
+spider.circle(20)
+
+# Configure spider legs 
+num_legs = 6
+length_legs = 70
+spider_angle = 380 / num_legs
+spider.pensize(5)
+repeat = 0
+
+# Draw spider legs 
+while (repeat < num_legs):
+  spider.goto(0,0)
+  spider.setheading(spider_angle*repeat)
+  spider.forward(length_legs)
+  repeat = repeat + 1
+
+spider.hideturtle()
+wn = trtl.Screen()
+wn.mainloop()
