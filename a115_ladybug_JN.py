9774fc3bbf233cd87918948b698213c4455a383d
jaybirdy07 <77217029+jaybirdy07@users.noreply.github.com>
2022-10-04 09:39:29 -0700

    Update a115_ladybug_JN.py

--- a115_ladybug_JN.py
+++ a115_ladybug_JN.py
@@ -1,14 +1,43 @@
 #   a116_ladybug.py
 import turtle as trtl
+ladybug = trtl.Turtle()
+
+# Configure ladybug legs 
+num_legs = 3
+length_legs = 50
+ladybug_angle = 120 / num_legs
+ladybug.pensize(5)
+repeat = 0
+
+# Draw ladybug legs
+while (repeat < num_legs):
+  ladybug.goto(0,-30)
+  ladybug.setheading(ladybug_angle*repeat)
+  ladybug.right(60)
+  ladybug.forward(length_legs)
+  repeat = repeat + 1
+
+ladybug.penup()
+num_legs = 3
+repeat = 0
+while (repeat < num_legs):
+  ladybug.goto(0,-30)
+  ladybug.pendown()
+  ladybug.setheading(-ladybug_angle*repeat)
+  ladybug.right(120)
+  ladybug.forward(length_legs)
+  repeat = repeat + 1
 
 # create ladybug head
-ladybug = trtl.Turtle()
+ladybug.penup()
+ladybug.goto(3, 15)
+ladybug.pendown()
 ladybug.pensize(40)
 ladybug.circle(5)
 
 # and body
 ladybug.penup()
-ladybug.goto(0, -55) 
+ladybug.goto(8, -10) 
 ladybug.color("red")
 ladybug.pendown()
 ladybug.pensize(40)
@@ -16,7 +45,7 @@ ladybug.circle(20)
 ladybug.setheading(270)
 ladybug.color("black")
 ladybug.penup()
-ladybug.goto(0, 5)
+ladybug.goto(0, 8)
 ladybug.pensize(2)
 ladybug.pendown()
 ladybug.forward(75)
