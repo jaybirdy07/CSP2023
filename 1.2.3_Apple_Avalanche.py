2f661369ad7bf6e949d57820e47f75a3d4ce3c4a
jaybirdy07 <77217029+jaybirdy07@users.noreply.github.com>
2022-11-29 15:20:57 -0800

    Update 1.2.3_Apple_Avalanche.py

--- 1.2.3_Apple_Avalanche.py
+++ 1.2.3_Apple_Avalanche.py
@@ -1,5 +1,6 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
@@ -7,36 +8,41 @@ apple_image = "apple.gif" # Store the file name of your shape
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-
 apple = trtl.Turtle()
-drawer = trtl.Turtle()
+apple_letter = 'a'
+letters = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
+
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
+  active_apple.penup()
   active_apple.shape(apple_image)
+  wn.tracer(False)
+  active_apple.sety(rand.randint(-25, 150)) #reset apple position
+  active_apple.setx(rand.randint(-125,125))
+  apple_letter = letters[rand.randint(0,8)]
+  active_apple.sety(active_apple.ycor()-30)
+  active_apple.color("white")
+  active_apple.write(apple_letter, align="center",font=("Arial", 40, "bold"))
+  active_apple.sety(active_apple.ycor()+30)
+  active_apple.showturtle()
+  wn.tracer(True)
   wn.update()
-
-def draw_an_A():
-  drawer.color("blue")
-  drawer.write("A", font=("Arial", 74, "bold"))
+  wn.onkeypress(apple_fall, apple_letter)
 
 def apple_fall():
   apple.penup()
+  apple.clear()
   apple.sety(-150)
-  
-def draw_letter():
-  drawer.pencolor("white")
-  drawer.penup()
-  drawer.goto(-250,100)
-  drawer.pendown()
-  drawer.write("A", font=("Arial", 74, "bold") )
+  apple.hideturtle()
+  draw_apple(apple)
+
+
   
 #-----function calls-----
 draw_apple(apple)
-draw_letter()
 
-wn.onkeypress(apple_fall, "a")
 wn.listen()
 wn.bgpic("background.gif")
 wn.mainloop()
